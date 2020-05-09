a2fc3348ca16908ed2f0351829465964e14ba506
renderz <464816158@qq.com>
2020-05-09 10:37:35 +0800

    init

---
+++ src/Table.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+require('antd/lib/table/style');
+var _Table = _interopDefault(require('antd/lib/table'));
+
+
+
+module.exports = _Table;
